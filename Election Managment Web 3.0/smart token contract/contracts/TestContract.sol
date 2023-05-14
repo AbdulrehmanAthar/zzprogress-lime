18d80c437ef6d6cdf0513da9b3909843d7906f0c
hadigithub777 <125210248+hadigithub777@users.noreply.github.com>
2023-05-14 23:50:49 +0500

    Add files via upload

---
+++ Election Managment Web 3.0/smart token contract/contracts/TestContract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TestContract
+{
+  uint public counter = 0;
+ 
+  constructor() public
+  {
+    IncrementCounter();
+  }
+ 
+  function IncrementCounter() public
+  {
+    counter ++;
+  }
+}
